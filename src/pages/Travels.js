023e159ec77ad41e1e57cea39c5d20cb08a6e738
shurik302 <balabuha1988@gmail.com>
2024-08-21 23:23:40 +0200

    fix

--- src/pages/Travels.js
+++ src/pages/Travels.js
@@ -1,28 +1,29 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
+import { Context } from '../';
 import moment from 'moment';
 import { useTranslation } from 'react-i18next';
 import '../stylesheets/Travels.css';
-import SadSmile from '../images/sad_smile.png';
 import axios from 'axios';
 import { Helmet } from 'react-helmet-async';
 
-const getCityNameById = (id, language, cities) => {
-  const city = cities.find(city => city.id === id);
-  return city ? (language === 'ua' ? city.ukrainian : city.value) : '';
-};
-
 function Travels() {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
   const now = moment();
-  const [trips, setTrips] = useState([]);
-  const [cities, setCities] = useState([]);
+  const [showAllActiveTrips, setShowAllActiveTrips] = useState(false);
+  const [activeTrips, setActiveTrips] = useState([]);
   const [isLoadingTrips, setIsLoadingTrips] = useState(true);
   const [error, setError] = useState(null);
+  const { store } = useContext(Context);
 
   const isTripActive = useCallback((trip) => {
     const tripArrivalDateTime = moment(trip.date_arrival).add(2, 'hours');
-    return now.isBefore(tripArrivalDateTime) && trip.isActive === 'активний';
+    return now.isBefore(tripArrivalDateTime);
   }, [now]);
+
+  const formatDate = (dateString) => {
+    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+    return new Date(dateString).toLocaleDateString('uk-UA', options);
+  };
 
   const fetchTrips = useCallback(async () => {
     try {
@@ -33,122 +34,98 @@ function Travels() {
         return;
       }
 
-      console.log('Attempting to fetch trips with accessToken:', accessToken);
-
       const response = await axios.get('https://bus-travel-release-7e3983a29e39.herokuapp.com/api/tickets/', {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
       });
 
-      console.log('Response data:', response.data);
-
       const trips = response.data;
-      const activeTrips = trips.filter(isTripActive).sort((a, b) => {
-        return moment(a.date_departure) - moment(b.date_departure);
-      });
+      const active = trips
+        .filter(trip => trip.isActive && isTripActive(trip))
+        .sort((a, b) => {
+          return moment(a.date_departure) - moment(b.date_departure);
+        });
 
-      setTrips(activeTrips);
+      setActiveTrips(active);
     } catch (error) {
+      setError('Error fetching trips');
       console.error('Error fetching trips:', error);
-      if (error.response) {
-        // Сервер ответил с ошибкой
-        console.error('Server responded with:', error.response.status, error.response.data);
-        setError(`Server error: ${error.response.status}`);
-      } else if (error.request) {
-        // Запрос был сделан, но ответа не было
-        console.error('No response received:', error.request);
-        setError('No response from server');
-      } else {
-        // Ошибка при настройке запроса
-        console.error('Error setting up request:', error.message);
-        setError(`Request error: ${error.message}`);
-      }
     } finally {
       setIsLoadingTrips(false);
     }
   }, [isTripActive]);
 
   useEffect(() => {
-    const fetchCities = async () => {
-      try {
-        console.log('Attempting to fetch cities...');
-        const response = await axios.get('https://bus-travel-release-7e3983a29e39.herokuapp.com/api/cities');
-        console.log('Cities data:', response.data);
-        setCities(response.data);
-      } catch (error) {
-        console.error('Error fetching cities:', error);
-      }
-    };
-
-    fetchCities();
-    fetchTrips();
-  }, [fetchTrips]);
-
-  const formatDate = (dateString) => {
-    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
-    return new Date(dateString).toLocaleDateString('uk-UA', options);
-  };
+    store.checkAuth().then(() => {
+      fetchTrips();
+    }).catch(err => {
+      console.error('Error during checkAuth:', err);
+      setError('Error during authentication');
+    });
+  }, [store, fetchTrips]);
 
   const groupTripsByDate = (trips) => {
     return trips.reduce((groupedTrips, trip) => {
-      const date = trip.date_departure.split('T')[0]; // Группировка по дате (без времени)
+      const date = trip.date_departure;
       if (!groupedTrips[date]) {
         groupedTrips[date] = [];
       }
       groupedTrips[date].push(trip);
       return groupedTrips;
     }, {});
+  }
+
+  const handleShowAllActiveTrips = () => {
+    setShowAllActiveTrips(true);
   };
 
-  const groupedTrips = groupTripsByDate(trips);
+  const groupedActiveTrips = groupTripsByDate(activeTrips);
 
-  const renderTrips = (groupedTrips) => {
-    return Object.keys(groupedTrips).map((date, index) => (
-      <div key={index} className="date-section">
-        <h2>{t('Travels_on')}: {formatDate(date)}</h2>
-        {groupedTrips[date].map((trip, tripIndex) => (
-          <div key={tripIndex} className='Trip'>
-            <div className='Date'>
-              <span>{formatDate(trip.date_departure)}</span>
+  const renderTrips = (groupedTrips, showAll) => {
+    const tripsArray = Object.keys(groupedTrips).reduce((acc, date) => [...acc, ...groupedTrips[date]], []);
+    const tripsToShow = showAll ? tripsArray : tripsArray.slice(0, 5);
+
+    return tripsToShow.map((trip, index) => (
+      <div key={index} className='Trip'>
+        <div className='Date'>
+          <span>{formatDate(trip.date_departure)}</span>
+        </div>
+        <div className='Downpart'>
+          <div className='MainInfo'>
+            <div className='Time'>
+              <div className='TimeDep'>
+                <span>{trip.departure}</span>
+              </div>
+              <div className='TimeArr'>
+                <span>{trip.arrival}</span>
+              </div>
             </div>
-            <div className='Downpart'>
-              <div className='MainInfo'>
-                <div className='Time'>
-                  <div className='TimeDep'>
-                    <span>{trip.departure}</span>
-                  </div>
-                  <div className='TimeArr'>
-                    <span>{trip.arrival}</span>
-                  </div>
-                </div>
-                <div className='RouteSymbol'>
-                  <div className='Line'></div>
-                  <div className='Circle top'></div>
-                  <div className='Circle bottom'></div>
-                </div>
-                <div className='Route'>
-                  <div className='From'>
-                    <div className='Place'>
-                      <span>{getCityNameById(trip.from, i18n.language, cities)}</span>
-                      <span>{trip.fromLocation}</span>
-                    </div>
-                  </div>
-                  <div className='To'>
-                    <div className='Place'>
-                      <span>{getCityNameById(trip.to, i18n.language, cities)}</span>
-                      <span>{trip.toLocation}</span>
-                    </div>
-                  </div>
+            <div className='RouteSymbol'>
+              <div className='Line'></div>
+              <div className='Circle top'></div>
+              <div className='Circle bottom'></div>
+            </div>
+            <div className='Route'>
+              <div className='From'>
+                <div className='Place'>
+                  <span>{trip.from}</span>
+                  <span>{trip.fromLocation}</span>
                 </div>
               </div>
-              <div className='AdditionalInfo'>
-                <span>{t('Baggage')}: {trip.baggage === "yes" ? t('Yes') : t('No')}</span>
-                <span>{t('Passengers')}: {trip.passengers}</span>
+              <div className='To'>
+                <div className='Place'>
+                  <span>{trip.to}</span>
+                  <span>{trip.toLocation}</span>
+                </div>
               </div>
             </div>
           </div>
-        ))}
+          <div className='AdditionalInfo'>
+            <span>{t('Baggage')}: {trip.baggage === "yes" ? t('Yes') : t('No')}</span>
+            <span>{t('Passengers')}: {trip.passengers}</span>
+          </div>
+        </div>
       </div>
     ));
   };
@@ -167,23 +144,19 @@ function Travels() {
         <title>{t('titles.travels')}</title>
       </Helmet>
       <div className='Welcome'><span>{t('WelcomeTravel')}</span></div>
-      <div className='FutureTravels FutureTravelsPage'>
-        <div className='Information'><span>{t('InformationTravels')}</span></div>
-        <div className='UserTravels'>
-          {Object.keys(groupedTrips).length > 0 ? (
-            <div className='HaveTravels'>
-              {renderTrips(groupedTrips)}
-            </div>
-          ) : (
-            <div className='DontHaveTravels'>
-              <div className='SadSmile'><img src={SadSmile} alt='Sad smile' /></div>
-              <div className='InfoHaveNotTravels'><span>{t('InfoHaveNotTravels')}</span></div>
-              <div className='ProposeBuyTicket'><span>{t('ProposeBuyTicket')}</span></div>
-              <a href='/map' className='ButtonBuyTickets'><span>{t('BuyTickets')}</span></a>
-            </div>
-          )}
+      {activeTrips.length === 0 ? (
+        <div className='ProposeBuyTicket ProposeBuyTicketAcc'>
+          <span>{t('ProposeBuyTicketAcc')}</span>
+          <a href='/map' className='ButtonBuyTickets ButtonBuyTicketsAcc'><span>{t('BuyTickets')}</span></a>
         </div>
-      </div>
+      ) : (
+        <>
+          {renderTrips(groupedActiveTrips, showAllActiveTrips)}
+          {activeTrips.length > 5 && !showAllActiveTrips && (
+            <button className='ShowAllButton' onClick={handleShowAllActiveTrips}><span>{t('ShowAllActiveTrips')}</span></button>
+          )}
+        </>
+      )}
     </div>
   );
 }
